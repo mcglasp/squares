d0d0a9edde277e773d38aaedb8b86dd9de9e5eb8
mcglasp <nmglasper@gmail.com>
2020-12-09 22:51:13 +0000

    Start on styling, as well as javascript/game-dependent styling (currently commented out. Timer still not functioning/resetting correctly

--- game.js
+++ game.js
@@ -5,12 +5,24 @@ var score = 0;
 var userClicks = [];
 var speed = 1000;
 var dots = 2;
-// var level = 0
+var timesUp = false
 
-// function initTimer(seconds){
-//  const milliseconds = seconds * 1000;
-//  setTimeout(updateTimer, milliseconds);
-// }
+function timer(){
+    var timesUp = true
+    console.log(timesUp)
+}
+
+function zeroTimer(){
+    var timesUp = false
+    console.log(timesUp)
+}
+
+function updateTimer(){
+    setTimeout(timer, 1000)
+}
+
+$(".cell").click(zeroTimer)
+
 
 
 
@@ -20,49 +32,35 @@ function updateScore(a, b) {
 }
 
 function endGame() {
-    console.log("you've reached endgame")
     var dots = 0;
     var speed = 1000;
     alert(`Soz, you lost :( Never mind, at least you scored ${score}`)
 }
 
 
-// function updateTimer() {
-//   var timesUp = true
-//   console.log(`now ${timesUp}`)
-// }
+
 
 
 function first() {
     var dots = 2
     var speed = 1000
     return dots
-    // return speed
-    // var initArray = [];
 }
 
 function levelUp() {
     dots++
-    // return speed-50
-    console.log("levelling up!")
-    console.log(`this many dots ${dots}`)
     resetArray()
-    // updateScore()
     return dots
 }
 
 function resetArray() {
     initArray = [];
-    console.log(`reset`)
-    console.log(initArray)
     resetClicks();
     return initArray;
 }
 
 function resetClicks() {
     userClicks = [];
-    console.log(`reset clicks`)
-    console.log(userClicks)
     newgame()
     return userClicks;
 }
@@ -72,39 +70,35 @@ function resetClicks() {
 function makeCircleArray(level) {
     var i = 0;
   do {
-    var val = Math.floor(Math.random() * 9)
+    var val = Math.floor(Math.random() * 8)
     initArray.push(`game-${val}`)
     i++
   }
   while (i < dots)
-  console.log(`${dots} ${initArray}`)
   return initArray;
 }
 
 
 function showCircle(item, j) {
   setTimeout(function () {
-      console.log(initArray)
     var num = initArray[j];
-    // console.log(num)
     var element = document.getElementById(num)
     element.style.backgroundColor = "red";
     window.setTimeout(function () {
       element.style.backgroundColor = "white";
     }, 500);
-    // console.log(initArray[j])
     j++;
   }, speed * j);
 };
 
 function showEachCircle(captureUserClicks) {
   initArray.forEach(showCircle);
+  updateTimer();
   }
 
 
 function captureUserClicks(clicked_id, callback) {
-    // console.log(userClicks)
-  userClicks.push(`game-${clicked_id}`);
+    userClicks.push(`game-${clicked_id}`);
   if (userClicks.length === initArray.length) {
   compareArraysClicks();
 }
@@ -115,7 +109,9 @@ function compareArraysClicks() {
   var gameArray = initArray.toString();
   if (gameArray === userArray) {
     updateScore(dots,score);
-    levelUp(); 
+    levelUp()
+  } else if (timesUp == true){
+      endGame()
   } else {
   updateScore(dots,score);
   endGame();
@@ -125,12 +121,21 @@ function compareArraysClicks() {
 // (userClicks.length === initArray.length && gameArray !== userArray) 
 
 function startGame() {
-    // let initArray = [];
+
     const firstLevel = makeCircleArray(first)
     showEachCircle(firstLevel, captureUserClicks)
 }
 
 function newgame(){
-    console.log("newgame here we are")
-  const whichLevel = makeCircleArray();
+    // changeColor(dots);
+    setTimeout(function() {
+    const whichLevel = makeCircleArray();
  showEachCircle(whichLevel, captureUserClicks)}
+  ,1000)};
+
+
+//   styling rules
+
+
+
+
