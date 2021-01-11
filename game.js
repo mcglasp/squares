962fc42c80f94bf2af29960cdfec9ff5dcb0c3ff
mcglasp <nmglasper@gmail.com>
2021-01-11 17:11:12 +0000

    timeControl function added. New game button adjusted so taht inital values are zeroed and new game starts correctly. 'start text' added. Some minor styling changes to button.

--- game.js
+++ game.js
@@ -1,74 +1,142 @@
-
+// Empty arrays
 let initArray = [];
+let userClicks = [];
+
+// Loop variables
 let i = 0;
 let j = 0;
-let score = 0;
-let userClicks = [];
+let x = 0;
+
+// Empty variables
 let k;
 let l;
-// let flashArray1 = [500, 600, 700, 800];
-// let flashArray2 = [450, 550, 650, 750];
-// let flashArray3 = [300, 380, 420, 600];
-// let flashArray4 = [200, 250, 300, 400];
-let x = 0;
-let colors = ['blue', 'pink', 'green'];
+let timer;
 
-let flashAudio = new Audio('sound.mp3');
-let padAudio = new Audio('sound.mp3');
-
-let timesUp = false;
-
+// Level control
+let score = 0;
 let level = {
     dots: 2,
     round: 0,
     speed: 800,
 };
 
-function padSound() {
-setTimeout(function() {padAudio.play(), 8
-})
+// UX & UI
+let colors = ['blue', 'pink', 'green'];
+let flashAudio = new Audio('sound.mp3');
+let padAudio = new Audio('sound.mp3');
+let scorebox = document.getElementById("score");
+let roundbox = document.getElementById("round");
+let typingText = document.getElementById("start-text");
+
+// Game order control
+function startGame() {
+// reset all initial values so that user can reset their own game without refreshing the page.
+    x = 0;
+    initArray = [];
+    userClicks = [];
+    score = 0;
+    level.dots = 2;
+    level.round = 0;
+    level.speed = 800;
+    scorebox.innerHTML = score;
+    roundbox.innerHTML = level.round;
+    clearTimer();
+    startText()
+    setTimeout(function() {
+    const firstLevel = makeCircleArray(level);
+    showEachCircle(firstLevel,timeControl);
+    }, 1500);
 }
 
-function speedUp() {
-level.speed-=50
-console.log(level.speed)
-return level.speed;
+function nextRound() {
+    console.log(`${level.dots} dots`);
+    console.log(`score is ${score}`);
+    setTimeout(function () {
+        const whichLevel = makeCircleArray(level);
+        showEachCircle(whichLevel,timeControl);
+    }, 1000);
+    showScore();
+    showRound();
 }
 
+function makeCircleArray(level) {
+    let i = 0;
+    var last_val = null;
+    do {
+        let val = Math.floor(Math.random() * 9);
+        if (val == last_val) continue; {
+            initArray.push(`game-${val}`);
+            i++;
+            last_val = val;
+        }
+    } while (i < level.dots);
+    return initArray;
+}
 
-
-
-// function speed() {
-//     let k = Math.floor(Math.random() * 4);
-//     if (level.round < 3) {
-//         console.log(flashArray1[k]);
-//         return flashArray1[k];
-//     } if (level.round >= 3 && level.round < 8) {
-//          console.log(flashArray2[k]);
-//         return flashArray2[k];
-//     } else if (level.round >= 8) {
-//           console.log(flashArray3[k]);
-//           return flashArray3[k];
-//         }
-//     }
+// Core gameplay functions (generally speaking, shown in order they are called)
     
+function showCircle(num, j) {
+    let k;
+    let y = x;
+    x++;
+    setTimeout(function () {
+        let color = colors[y % colors.length];
+        let num = initArray[j];
+        let element = document.getElementById(num);
+        element.classList.add(`cell-glow-${color}`);
+        padSound();
+        window.setTimeout(function () {
+            element.classList.remove(`cell-glow-${color}`);
+        }, 300);
+        j++;
+    }, level.speed * j);
+}
 
+function showEachCircle(cb1, cb2) {
+    initArray.forEach(showCircle);
+    cb2()
+}
+
+function timeControl(){
+let counter = 5;
+    timer = setInterval(function() {
+        if (counter > 0) {
+            counter--;
+            // console.log("counting down"+counter)          
+        } else {
+            endGame();
+            clearTimer();
+        }
+    }, 1000);
+}
+
+function clearTimer() {
+clearInterval(timer)
+    }
+    
+function captureUserClicks(clicked_id) {
+    userClicks.push(`game-${clicked_id}`);
+    if (userClicks.length === initArray.length) {
+        compareArraysClicks()
+        clearTimer()
+    }
+}
+
+function compareArraysClicks() {
+    let userArray = userClicks.toString();
+    let gameArray = initArray.toString();
+    if (gameArray === userArray) {
+        levelUp();
+    } else {
+        endGame();
+    }
+}
 
 function endGame() {
     alert(`You lost :( Never mind, at least you scored ${score}`);
 }
 
-function showScore() {
-    let scorebox = document.getElementById("score");
-    scorebox.innerHTML = score;
-}
-
-function showRound() {
-    let roundbox = document.getElementById("round");
-    roundbox.innerHTML = level.round;
-}
-
-
+// Level control & reset functions
 
 function levelUp() {
     score = score += 5;
@@ -79,8 +147,12 @@ function levelUp() {
     }
     resetGameArray();
     resetUserClicks(nextRound);
-
     return level.dots + level.round + score;
+}
+
+function speedUp() {
+    level.speed -= 50
+    return level.speed;
 }
 
 function resetGameArray() {
@@ -94,82 +166,28 @@ function resetUserClicks(callback) {
     return userClicks;
 }
 
+// UX & UI functions
 
-
-
-function makeCircleArray(level) {
-    let i = 0;
-    var last_val = null;
-    do {
-        let val = Math.floor(Math.random() * 9);
-        if (val == last_val) continue; {
-            initArray.push(`game-${val}`);
-            i++;
-            last_val = val;
-        }
-    } while (i < level.dots);
-    console.log(`COMPUTER-${initArray}`);
-    return initArray;
+function showScore() {
+    // let scorebox = document.getElementById("score");
+    scorebox.innerHTML = score;
 }
 
-function showCircle(num, j) {
-    let k;
-    let y = x;
-    x++;
+function showRound() {
+    roundbox.innerHTML = level.round;
+}
+
+function padSound() {
     setTimeout(function () {
-        let color = colors[y % colors.length];
-        let num = initArray[j];
-        let element = document.getElementById(num);
-        element.classList.add(`cell-glow-${color}`);
-        padSound();
-        window.setTimeout(function () {
-            element.classList.remove(`cell-glow-${color}`);
-            
-        }, 300);
-        j++;
+        padAudio.play(), 8
+    })
+};
 
-    }, level.speed * j);
-    
-}
+function startText() {
+    typingText.innerHTML = "ok, let's go";
 
-function showEachCircle(captureUserClicks) {
-    initArray.forEach(showCircle);
-}
-
-
-function captureUserClicks(clicked_id) {
-    userClicks.push(`game-${clicked_id}`);
-    if (userClicks.length === initArray.length) {
-        compareArraysClicks();
-        console.log(`user ${userClicks}`);
-    }
-}
-
-function compareArraysClicks() {
-    let userArray = userClicks.toString();
-    let gameArray = initArray.toString();
-    if (gameArray === userArray) {
-        levelUp();
-    } else if (timesUp == true) {
-        endGame();
-    } else {
-        endGame();
-    }
 }
 
 
 
 
-function startGame() {
-    const firstLevel = makeCircleArray(level);
-    showEachCircle(firstLevel, captureUserClicks);
-}
-
-function nextRound() {
-    setTimeout(function () {
-        const whichLevel = makeCircleArray(level);
-        showEachCircle(whichLevel, captureUserClicks);
-    }, 1000);
-        showScore();
-        showRound();
-}
