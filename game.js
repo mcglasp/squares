ac22339707ba9602bf2139b37d13cd2999b98503
mcglasp <nmglasper@gmail.com>
2021-01-25 18:21:58 +0000

    Minor spacing issues addressed

--- game.js
+++ game.js
@@ -240,7 +240,7 @@ for (k = 0; k < information.length; k++) {
 // Light theme toggle
 
 $('.slider').click(function(){
-    $("body, .container, .container-info, #logo, #newgame, #user-game-container, #game-container, .center-text, .display-box, #info-text").toggleClass("light-theme");
+    $("body, .container, .container-info, #logo, #newgame, #user-game-container, #game-container, .center-text, .display-box, #info-text, sup").toggleClass("light-theme");
 })
 
 // Hard mode toggle
